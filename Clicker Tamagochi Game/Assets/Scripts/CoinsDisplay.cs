8b9bb9f48835941646ca5f7e75d570733b2877bd
jonter <jonterrusian@gmail.com>
2025-11-29 01:25:57 +0300

    v 0.6 Polish anim logic for player and add coins display

---
+++ Clicker Tamagochi Game/Assets/Scripts/CoinsDisplay.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using DG.Tweening;
+
+public class CoinsDisplay : MonoBehaviour
+{
+    TMP_Text coinsText;
+
+    [SerializeField] float jumpAnimTime = 0.1f;
+    [SerializeField] float jumpHeight = 20;
+    Vector3 startTextPos;
+
+    private void OnEnable()
+    {
+        coinsText = GetComponentInChildren<TMP_Text>();
+        GameData.Instance.OnCoinsAdd += DisplayCoins;
+        startTextPos = coinsText.rectTransform.localPosition;   
+        GameData.Instance.AddCoins(0);
+    }
+
+    private void OnDisable()
+    {
+        GameData.Instance.OnCoinsAdd -= DisplayCoins;
+        coinsText.rectTransform.DOKill();
+    }
+
+    void DisplayCoins(float coins)
+    {
+        coinsText.text = "" + coins;
+        AnimText();
+    }
+
+    void AnimText()
+    {
+        coinsText.rectTransform.DOKill();
+        coinsText.rectTransform.localPosition = startTextPos;
+        coinsText.rectTransform.DOLocalMoveY(jumpHeight, jumpAnimTime)
+            .SetLoops(2, LoopType.Yoyo);
+    }
+
+    
+
+
+}
